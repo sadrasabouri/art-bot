6e43593c5691506cc616781237ec382d882cfbd0
sadrasabouri <sabouri.sadra@gmail.com>
2021-09-30 16:47:26 +0330

    add : autopep8 files added.

---
+++ autopep8.bat
@@ -0,0 +1,1 @@
+python -m autopep8 main.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
---
+++ autopep8.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python -m autopep8 main.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
