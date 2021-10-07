71f205bdbd6203570b67c9380b5dad7e2cafac17
sadrasabouri <sabouri.sadra@gmail.com>
2021-10-07 20:29:23 +0330

    add : main.py added.

---
+++ main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from art import *
+
+from telegram import InlineKeyboardMarkup, ChatAction
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+
+import logging
+
+from params import *
+from functions import make_outputs_dir, art_start
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+
+    make_outputs_dir()
+    updater = Updater(Token)
+    updater.dispatcher.add_handler(CommandHandler("start", art_start))
+
+    updater.start_polling()
+    updater.idle()
