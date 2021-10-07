b60033d987cd30849d59e511ff9c93531bbc78ee
sadrasabouri <sabouri.sadra@gmail.com>
2021-10-07 19:26:44 +0330

    add : functions.py added.

---
+++ functions.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import os
+
+def make_outputs_dir():
+    """
+    Make outputs/ directory in current directory.
+
+    :return: None
+    """
+    if "outputs" not in os.listdir():
+        os.mkdir("outputs")
