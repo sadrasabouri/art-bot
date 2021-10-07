b5e85c0d1b91ee790fe70e9aae662d23518a796e
sadrasabouri <sabouri.sadra@gmail.com>
2021-10-07 20:49:09 +0330

    add : command_handler added.

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@
 import os
 from telegram import InlineKeyboardMarkup
 
-from params import KeyboardMenu, StartText
+from params import KeyboardMenu, StartText, HelpMessage, WarningMessage
 
 def make_outputs_dir():
     """
@@ -25,3 +25,20 @@ def art_start(update, context):
     """
     reply_markup_menu = InlineKeyboardMarkup(KeyboardMenu)
     update.message.reply_text(StartText, reply_markup=reply_markup_menu)
+
+def command_handler(update, context):
+    '''
+    Check callback query response and run handlers.
+
+    :param update: telegram update object
+    :type update: telegram.update
+    :param context: telegram context object
+    :type context: CallbackContext
+    :return: None
+    '''
+    query = update.callback_query
+    user_id = query.from_user.id
+    if query.data == "help":
+        context.bot.send_message(user_id, HelpMessage)
+    else:
+        context.bot.send_message(user_id, WarningMessage)
